775aa673d3ce340ab84659df06dc3059d800f104
nitzano707 <nitzan.orot@gmail.com>
2024-11-28 17:28:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ from faster_whisper import WhisperModel
 
 app = FastAPI()
 
-# טוען את מודל התמלול
-model = WhisperModel("ivrit-ai/faster-whisper-v2-d4", device="cpu")
+model = WhisperModel("ivrit-ai/faster-whisper-v2-d4", device="cuda")
 
 @app.post("/transcribe/")
 async def transcribe(file: UploadFile = File(...)):
-    # שמירת קובץ האודיו
     with open(file.filename, "wb") as f:
         f.write(await file.read())
 
-    # תמלול האודיו
     segments, _ = model.transcribe(file.filename, language="he")
     transcription = [{"start": seg.start, "end": seg.end, "text": seg.text} for seg in segments]
 
